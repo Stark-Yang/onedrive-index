f3d1dd6815c5be58d448224e6a4dddfb9d322037
Stark-Yang <56785795+Stark-Yang@users.noreply.github.com>
2024-09-04 16:14:09 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -9,7 +9,7 @@ module.exports = {
   // and the writing format is 'iconPrefix-iconName', e.g. icon: 'fab-github',
   // If the FontAwesomIcon you choose does not display properly, you may need to import it in `src/pages/_app.tsx`.
   // However, the browser's tab bar icon is STILL determined by /public/favicon.ico
-  icon: 'fas-cube', // or icon: '/icons/128.png', to use a image.
+  icon: '32.jpg', // or icon: '/icons/128.png', to use a image.
 
   // Prefix for KV Storage.
   // You can put this in Vercel's environment variable 'KV_PREFIX' without any modification here.
